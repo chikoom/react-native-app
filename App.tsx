d4e0a0f04772fdc23a2ffb83d8ddab8a95d75515
Chikoom <callforcopy@gmail.com>
2021-04-15 19:58:47 +0300

    feature | built initial layout

--- App.tsx
+++ App.tsx
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={appWrapper}>
+      <View style={topInputWrapper}>
+        <TextInput placeholder='Enter Reminder!' style={topInput} />
+        <Button title='Add' onPress={() => {}} />
+      </View>
+      <View></View>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const { topInput, appWrapper, topInputWrapper } = StyleSheet.create({
+  topInput: {
+    flex: 0.9,
+    padding: 5,
+    borderBottomColor: 'black',
+    borderBottomWidth: 2,
+    marginBottom: 10
   },
-});
+  topInputWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  appWrapper: { paddingTop: 60, paddingBottom: 30, paddingHorizontal: 20 }
+})
